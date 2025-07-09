fec7be56c8dcadd65e00587b1b202cc519edf4b6
Akib-dev1 <akibanj2017@gmail.com>
2025-07-10 02:06:52 +0600

    Manage Users and Managing user functionality Done

--- src/Dashboard Pages/ManageUsers.jsx
+++ src/Dashboard Pages/ManageUsers.jsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { ScaleLoader } from "react-spinners";
+import Swal from "sweetalert2";
 
 const ManageUsers = () => {
   const { data, isLoading, refetch } = useQuery({
@@ -13,6 +14,33 @@ const ManageUsers = () => {
     },
   });
   refetch();
+  const handleMakeAdmin = async (userEmail) => {
+    const response = await axios.patch(
+      `http://localhost:5000/users/${userEmail}`,
+      { role: "admin" }
+    );
+    if (response.data.modifiedCount > 0) {
+      Swal.fire({
+        title: "User role updated!",
+        icon: "success",
+      });
+      refetch();
+    }
+  };
+
+  const handleMakePremium = async (userEmail) => {
+    const response = await axios.patch(
+      `http://localhost:5000/users/${userEmail}`,
+      { role: "premium" }
+    );
+    if (response.data.modifiedCount > 0) {
+      Swal.fire({
+        title: "User is now a premium member!",
+        icon: "success",
+      });
+      refetch();
+    }
+  };
   if (isLoading) {
     return (
       <div className="flex justify-center items-center h-full">
@@ -48,18 +76,24 @@ const ManageUsers = () => {
                         Admin
                       </span>
                     ) : (
-                      <Button className="bg-[#D33454] hover:bg-[#b72b48] text-white text-sm">
+                      <Button
+                        className="bg-[#D33454] hover:bg-[#b72b48] text-white text-sm cursor-pointer"
+                        onClick={() => handleMakeAdmin(user.email)}
+                      >
                         Make Admin
                       </Button>
                     )}
                   </td>
                   <td className="px-4 py-3 text-center">
-                    {user.premium ? (
+                    {user.role === "premium" ? (
                       <span className="text-yellow-600 font-semibold">
                         Premium
                       </span>
                     ) : (
-                      <Button className="bg-[#E3D4B4] hover:bg-[#cbb88f] text-[#5A3B3B] text-sm">
+                      <Button
+                        className="bg-[#E3D4B4] hover:bg-[#cbb88f] text-[#5A3B3B] text-sm cursor-pointer"
+                        onClick={() => handleMakePremium(user.email)}
+                      >
                         Make Premium
                       </Button>
                     )}
